15175d8d70994361e0e6ccde6cce4c34cfc3cd1f
Juliana <jumiranda0501@gmail.com>
2022-12-12 18:30:07 -0300

    Add slider to change squares size

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-// Create 16x16 squares inside draw container
+// Change square size
+var slider = document.getElementById("slider");
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    console.log(this.value)
+
+    // clear draw container
+    const drawContainer = document.querySelector('#draw-container');
+    drawContainer.textContent = '';
+
+    // Add new squares
+    createDrawSquares(this.value)
+}
+
+// Create squares inside draw container default = 16x16
 function createDrawSquares(cols=16) {
     const drawContainer = document.querySelector('#draw-container');
     const squares =  cols * cols
 
     for (let i = 0; i < squares; i++) {
+        // Add container square
         const square = document.createElement('div');
-        const size = 400 / 16;
+        const size = 400 / cols;
         square.setAttribute('style', `width: ${size}px; height: ${size}px;`); 
         drawContainer.appendChild(square);
 
@@ -22,6 +38,4 @@ function createDrawSquares(cols=16) {
     }
 }
 
-
 createDrawSquares();
-
