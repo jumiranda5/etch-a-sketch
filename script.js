bdfa8d7d3ac698333a0f84c01718946e7b1270e8
Juliana <jumiranda0501@gmail.com>
2022-12-12 18:03:06 -0300

    Add function to draw on container

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 // Create 16x16 squares inside draw container
+function createDrawSquares(cols=16) {
+    const drawContainer = document.querySelector('#draw-container');
+    const squares =  cols * cols
 
-const drawContainer = document.querySelector('#draw-container');
+    for (let i = 0; i < squares; i++) {
+        const square = document.createElement('div');
+        const size = 400 / 16;
+        square.setAttribute('style', `width: ${size}px; height: ${size}px;`); 
+        drawContainer.appendChild(square);
 
-const squares =  16 * 16
-for (let i = 0; i < squares; i++) {
-    const square = document.createElement('div');
-    drawContainer.appendChild(square)
+        // Change square background color on mouse click and drag
+        let mouseDown = false;
+        drawContainer.addEventListener('mousedown', () => mouseDown = true);
+        drawContainer.addEventListener('mouseup', () => mouseDown = false);
+        square.addEventListener('mouseenter', (e) => {
+            if (mouseDown) {
+                e.target.style.background = 'black';
+            }
+        });
+        square.addEventListener('click', (e) => e.target.style.background = 'black');
+    }
 }
+
+
+createDrawSquares();
+
