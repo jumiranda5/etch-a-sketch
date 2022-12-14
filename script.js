c94d5c39b73c0b243c02226375afac57dc470e50
Juliana <jumiranda0501@gmail.com>
2022-12-14 18:16:04 -0300

    Add eraser button function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+'use strict';
+
 let columns = 16;
+let eraseSelected = false;
+let selectedColor = "#000000"
+
+createDrawSquares();
 
 // Change square size (each time you drag the slider handle)
 const slider = document.getElementById("slider");
@@ -11,10 +17,8 @@ slider.oninput = function() {
 // Buttons
 const btnClear = document.querySelector('#clear');
 const btnErase = document.querySelector('#erase');
-
 btnClear.addEventListener('click', () => clear());
-
-//btnErase.addEventListener('click', () => TODO);
+btnErase.addEventListener('click', () => erase());
 
 
 // Create squares inside draw container default = 16x16
@@ -29,18 +33,7 @@ function createDrawSquares(cols=16) {
         square.setAttribute('style', `width: ${size}px; height: ${size}px;`); 
         drawContainer.appendChild(square);
 
-        // Change square background color on mouse click and drag
-        let mouseDown = false;
-        drawContainer.addEventListener('mousedown', () => mouseDown = true);
-        drawContainer.addEventListener('mouseup', () => mouseDown = false);
-        drawContainer.addEventListener('mouseleave', () => mouseDown = false);
-        square.addEventListener('mouseenter', (e) => {
-            if (mouseDown) {
-                e.target.style.background = 'black';
-            }
-        });
-        square.addEventListener('click', (e) => e.target.style.background = 'black');
-        
+        setSquareColorChange(square, selectedColor);        
     }
 }
 
@@ -51,4 +44,42 @@ function clear() {
     createDrawSquares(columns)
 }
 
-createDrawSquares();
+// Change squares backgrounds back to white
+function erase() {
+
+    const squareList = document.querySelectorAll("#draw-container > div");
+
+    if (eraseSelected) {
+        eraseSelected = false;
+        btnErase.style.background = "#e8e8e8";
+        for (let i = 0; i < squareList.length; i++) {
+            setSquareColorChange(squareList[i], selectedColor);
+        }
+    }
+    else {
+        eraseSelected = true;
+        btnErase.style.background = "#f1e776";
+        for (let i = 0; i < squareList.length; i++) {
+            setSquareColorChange(squareList[i], "#FFFFFF");
+        }
+    }
+
+}
+
+// Set square to change background color on mouse click and drag
+function setSquareColorChange(square, color) {
+
+    const drawContainer = document.querySelector('#draw-container');
+
+    let mouseDown = false;
+    drawContainer.addEventListener('mousedown', () => mouseDown = true);
+    drawContainer.addEventListener('mouseup', () => mouseDown = false);
+    drawContainer.addEventListener('mouseleave', () => mouseDown = false);
+    square.addEventListener('mouseenter', (e) => {
+        if (mouseDown) {
+            e.target.style.background = color;
+        }
+    });
+    square.addEventListener('click', (e) => e.target.style.background = color);
+
+}
