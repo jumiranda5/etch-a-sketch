e8c5b7cf880d057906e63f62f320f860f178f1a3
Juliana <jumiranda0501@gmail.com>
2022-12-14 19:27:14 -0300

    Add color picker value

--- script.js
+++ script.js
@@ -2,17 +2,33 @@
 
 let columns = 16;
 let eraseSelected = false;
-let selectedColor = "#000000"
+let selectedColor = "#000000";
 
+// Draw container
+const drawContainer = document.querySelector('#draw-container');
+let mouseDown = false;
+drawContainer.addEventListener('mousedown', () => mouseDown = true);
+drawContainer.addEventListener('mouseup', () => mouseDown = false);
+drawContainer.addEventListener('mouseleave', () => mouseDown = false);
+
+// Init draw container creating columns and rows
 createDrawSquares();
 
 // Change square size (each time you drag the slider handle)
 const slider = document.getElementById("slider");
+slider.onmousemove = (e) => columns = e.target.value;
+slider.onchange = (e) => clear();
 
-slider.oninput = function() {
-    columns = this.value;
-    clear();
-}
+
+// Color input
+const colorPicker = document.querySelector('#color-picker');
+colorPicker.addEventListener("input", (e) => {
+    selectedColor = e.target.value;
+    const squareList = document.querySelectorAll("#draw-container > div");
+    for (let i = 0; i < squareList.length; i++) {
+        setSquareColorChange(squareList[i], selectedColor);
+    }
+});
 
 // Buttons
 const btnClear = document.querySelector('#clear');
@@ -23,7 +39,6 @@ btnErase.addEventListener('click', () => erase());
 
 // Create squares inside draw container default = 16x16
 function createDrawSquares(cols=16) {
-    const drawContainer = document.querySelector('#draw-container');
     const squares =  cols * cols
 
     for (let i = 0; i < squares; i++) {
@@ -39,7 +54,6 @@ function createDrawSquares(cols=16) {
 
 // Rewrite draw container
 function clear() {
-    const drawContainer = document.querySelector('#draw-container');
     drawContainer.textContent = '';
     createDrawSquares(columns)
 }
@@ -69,12 +83,8 @@ function erase() {
 // Set square to change background color on mouse click and drag
 function setSquareColorChange(square, color) {
 
-    const drawContainer = document.querySelector('#draw-container');
+    console.log("...");
 
-    let mouseDown = false;
-    drawContainer.addEventListener('mousedown', () => mouseDown = true);
-    drawContainer.addEventListener('mouseup', () => mouseDown = false);
-    drawContainer.addEventListener('mouseleave', () => mouseDown = false);
     square.addEventListener('mouseenter', (e) => {
         if (mouseDown) {
             e.target.style.background = color;
@@ -82,8 +92,7 @@ function setSquareColorChange(square, color) {
     });
     square.addEventListener('click', (e) => e.target.style.background = color);
 
-    // Prevent mouse down and up events on square div => it conflicts with the parent div events
+    // Prevent mouse down events on square div => it conflicts with the parent div events
     square.addEventListener('mousedown', (e) => e.preventDefault());
-    square.addEventListener('mouseup', (e) => e.preventDefault());
 
 }
